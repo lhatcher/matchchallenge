4006c33400cf904881615a3b0682b5f393477a5c
Liam Hatcher <hatcherpiano@gmail.com>
2016-07-15 18:01:28 -0700

    Create tweet_analysis endpoint

--- server/server.js
+++ server/server.js
@@ -23,28 +23,71 @@ const twitter = new Twitter({
   access_token_secret: API_KEYS.TWITTER.access_token_secret,
 });
 
-app.get('/api/tweets', (req, res) => {
+var indico = require('indico.io');
+indico.apiKey = API_KEYS.INDICO_API_KEY;
+
+
+// This endpoint grabs the most recent tweets for a specified username
+// using Twitter's API, and then determines personality type using Indico's
+// Natural Language Processing API.
+app.get('/api/tweet_analysis', (req, res) => {
   'use strict';
   let username = req.query.username;
   let params = {screen_name: username, exclude_replies: true};
 
   twitter.get('statuses/user_timeline', params, (err, tweets, response) => {
     if (!err) {
-      let arr = [];
+      let tweetData = [];
 
       tweets.forEach( (tweet) => {
-        arr.push(tweet.text);
+        tweetData.push(tweet.text);
       });
 
-      res.json({
-        tweets: arr,
+      // create object to store average weight of each persona
+      let personas = {};
+
+      indico.batchPersonas(tweetData).then( (personaData) => {
+        // create initial key value pairs
+        console.log(personaData[0])
+
+        for ( let persona in personaData[0] ) {
+          personas[persona] = 0;
+        }
+
+        // iterate through batch output
+        personaData.forEach( (persona) => {
+          // iterate through each persona value
+          for ( let type in persona ) {
+            // update running total
+            personas[type] = personas[type] + persona[type];
+          }
+        });
+        console.log(personas);
+
+      }).then( () => {
+        // get largest value
+        let MBTI_type = '';
+        let max = 0;
+        Object.keys(personas).forEach( type => {
+          MBTI_type = personas[type] > max ? type : MBTI_type;
+          max = Math.max(personas[type], max);
+        });
+
+        res.json({
+          personalityType: MBTI_type,
+          tweets: tweetData,
+        });
       });
+
 
     } else {
       console.log(err)
     }
   });
 });
+
+
+
 
 
 app.listen(3000, () => {
